4e81d5b3360bddeb9b4ae720adbc21f92b0901e4
sivisiviku <sivisiviku@gmail.com>
2023-09-30 23:40:20 +0700

    result summary desktop view finish

--- src/pages/js/ResultSummary.js
+++ src/pages/js/ResultSummary.js
@@ -1,4 +1,8 @@
 import '../css/result_summary.css'
+import reactionIcon from '../../assets/result_summary/images/icon-reaction.svg'
+import memoryIcon from '../../assets/result_summary/images/icon-memory.svg'
+import verbalIcon from '../../assets/result_summary/images/icon-verbal.svg'
+import visualIcon from '../../assets/result_summary/images/icon-visual.svg'
 
 function ResultSummary() {
     return <div className="container">
@@ -9,26 +13,44 @@ function ResultSummary() {
                     <h1>76</h1>
                     <p>of 100</p>
                 </div>
-                <h2>Great</h2>
-                <p>You scored higher than 65% of the people who have taken these tests.</p>
+                <div className='score-description'>
+                    <h2>Great</h2>
+                    <p>You scored higher than 65% of the people who have taken these tests.</p>
+                </div>
             </div>
             <div className='summary'>
-                <h3>Summary</h3>
-                <div className='reaction'>
-                    <p>Reaction</p>
-                    <p>80/100</p>
+                <div className='summary-title-wrapper'>
+                    <h3>Summary</h3>
                 </div>
-                <div className='memory'>
-                    <p>Reaction</p>
-                    <p>92/100</p>
-                </div>
-                <div className='verbal'>
-                    <p>Reaction</p>
-                    <p>61/100</p>
-                </div>
-                <div className='visual'>
-                    <p>Reaction</p>
-                    <p>72/100</p>
+                <div className='summary-detail-wrapper'>
+                    <div className='reaction'>
+                        <div className='summary-detail-tittle'>
+                            <img src={reactionIcon} />
+                            <p>Reaction</p>
+                        </div>
+                        <p><span className='actual-score'>80</span><span className='max-score'> / 100</span></p>
+                    </div>
+                    <div className='memory'>
+                        <div className='summary-detail-tittle'>
+                            <img src={memoryIcon} />
+                            <p>Memory</p>
+                        </div>
+                        <p><span className='actual-score'>92</span><span className='max-score'> / 100</span></p>
+                    </div>
+                    <div className='verbal'>
+                        <div className='summary-detail-tittle'>
+                            <img src={verbalIcon} />
+                            <p>Verbal</p>
+                        </div>
+                        <p><span className='actual-score'>61</span><span className='max-score'> / 100</span></p>
+                    </div>
+                    <div className='visual'>
+                        <div className='summary-detail-tittle'>
+                            <img src={visualIcon} />
+                            <p>Visual</p>
+                        </div>
+                        <p><span className='actual-score'>72</span><span className='max-score'> / 100</span></p>
+                    </div>
                 </div>
                 <div className='button-wrapper'>
                     <button className='button'>Continue</button>
